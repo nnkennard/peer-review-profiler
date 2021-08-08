1ff17af0568320cb54c4dbb75b2665f02bd66890
Srimoyee <srimoyeebhattacharyya@gmail.com>
2021-08-08 00:40:47 -0500

    Add separate virtual env for specificity features

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 #module load python3/3.9.0-2010
 
-#python3 -m venv ve
-#source ve/bin/activate
-#python -m pip install --upgrade pip
-#python -m pip install -r requirements.txt
+python3 -m venv ve
+source ve/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
-#git submodule update --init --recursive
-#git submodule update --init --recursive # I don't know if these needs to be run twice? But I've had to
+git submodule update --init --recursive
+git submodule update --init --recursive # I don't know if these needs to be run twice? But I've had to
 
 cd ReviewAdvisor/
 bash download_tagger.sh
@@ -17,15 +17,26 @@ cd tagger
 ln -s ../seqlab_final/
 sed -i 's/is_world_master/is_world_process_zero/' run_tagger.py
 
-#cd ../../
+cd ../../
 
-#wget https://nlp.stanford.edu/data/glove.840B.300d.zip
-#unzip glove.840B.300d.zip
-#rm glove.840B.300d.zip
-#mv glove.840B.300d.txt Domain-Agnostic-Sentence-Specificity-Prediction
+wget https://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip glove.840B.300d.zip
+rm glove.840B.300d.zip
+mv glove.840B.300d.txt Domain-Agnostic-Sentence-Specificity-Prediction
 
-#mkdir -p models/argument
+mkdir -p models/argument
 # ??? Where to get the SciBert model???
+# https://drive.google.com/file/d/1-6QMv3NahTukRniYyc_RGBkWzxrkyDTw/view?usp=sharing
+cd models/argument
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-6QMv3NahTukRniYyc_RGBkWzxrkyDTw' -O 'SciBert.model'
+cd ../../
 
+
+deactivate
+python3 -m venv ve_specificity
+source ve_specificity/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements_specificity.txt
 # This is for Convokit
-#python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm
+deactivate
